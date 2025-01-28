b98d51dd96a45232625b14b9b9c6e1f4d3a5a343
Mille <182352340+Mille-code9@users.noreply.github.com>
2025-01-28 19:05:09 +0100

    Test to brake, gas and currentSpeed

---
+++ src/test/Saab95Test.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Saab95Test {
+    private Saab95 vehicle;
+
+    @BeforeEach
+    public void setup(){
+        vehicle = new Saab95();
+    }
+    @Test
+    void testCurrentSpeedInRange(){
+        vehicle.gas(0.5); //Control the speed
+        double currentSpeed = vehicle.getCurrentSpeed();
+        //speed can only be in the range [0,enginePower]
+        assertTrue(currentSpeed >= 0 && currentSpeed <= vehicle.getEnginePower());
+    }
+    @Test
+    void testGasGiveCorrectChangeOffSpeed(){
+        double initialSpeed = vehicle.getCurrentSpeed();
+        vehicle.gas(0.7);
+        double currentSpeed = vehicle.getCurrentSpeed();
+        assertTrue(initialSpeed <= currentSpeed, "The speed can not be reducing when gas");
+    }
+    @Test
+    void testBrakeGiveCorrectChangeOffSpeed(){
+        double initialSpeed = vehicle.getCurrentSpeed();
+        vehicle.brake(0.7);
+        double currentSpeed = vehicle.getCurrentSpeed();
+        assertTrue(initialSpeed >= currentSpeed, "The speed can not be increasing when braking");
+    }
+    @Test
+    void testGasWithValidAmount() {
+        //Only accepts values in the range [0,1]
+        assertThrows(IllegalAccessError.class, () -> vehicle.gas(-0.1));
+        assertThrows(IllegalAccessError.class, () -> vehicle.gas(1.1));
+    }
+
+    @Test
+    void testBrakeWithValidAmount() {
+        //Only accepts values in the range [0,1]
+        assertThrows(IllegalAccessError.class, () -> vehicle.brake(-0.1));
+        assertThrows(IllegalAccessError.class, () -> vehicle.brake(1.1));
+    }
+}

da45309938afc114e9fca43c095d1728d84c7f2e
NaolenBaolen <noelagustin@live.com>
2025-01-28 15:11:20 +0100

    Car class changed so it contains position for subclasses, Volvo written as extension from Car

--- src/Car.java
+++ src/Car.java
@@ -7,11 +7,21 @@ public class Car {
     private Color color;
     private final String modelName;
 
+    //position and direction
+    private double x;
+    private double y;
+    private double angle; //direction car is facing in degrees
+
     public Car (int nDoors, double enginePower, Color color, String modelName){
         this.nDoors = nDoors;
         this.enginePower = enginePower;
         this.color = color;
         this.modelName = modelName;
+
+        this.x = 0; //initial position
+        this.y = 0;
+        this.angle = 0;
+
         stopEngine();
     }
 //following is the shared functionality methods
@@ -20,6 +30,10 @@ public class Car {
     public double getEnginePower(){return enginePower;}
 
     public double getCurrentSpeed() {return currentSpeed;}
+
+    public void setCurrentSpeed(double currentSpeed){
+        this.currentSpeed = currentSpeed;
+    }
 
     public Color getColor() {return color;}
 
@@ -30,11 +44,11 @@ public class Car {
     public void stopEngine(){currentSpeed = 0;}
 
     public void gas(double amount){
-        //delegate this implementation to the subclass (preferebly with abstract method)
+        //delegate this implementation to the subclass
     }
 
     public void brake(double amount){
-        //delegate this implementation to the subclass (aslo preferably as abstract method)
+        //delegate this implementation to the subclass
     }
 
 }
--- src/Volvo240.java
+++ src/Volvo240.java
@@ -1,59 +1,39 @@
 import java.awt.*;
 
-public class Volvo240{
+public class Volvo240 extends Car implements SpeedHandeling, Movable{
 
-    public final static double trimFactor = 1.25;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
+    public final static double trimFactor = 1.25;    //specific for volvo hence it get to stay
+//    public int nrDoors; // Number of doors on the car
+//    public double enginePower; // Engine power of the car
+//    public double currentSpeed; // The current speed of the car
+//    public Color color; // Color of the car
+//    public String modelName; // The car model name
     
+//    public Volvo240(){
+//        nrDoors = 4;
+//        color = Color.black;
+//        enginePower = 100;
+//        modelName = "Volvo240";
+//        stopEngine();
+//    }
     public Volvo240(){
-        nrDoors = 4;
-        color = Color.black;
-        enginePower = 100;
-        modelName = "Volvo240";
-        stopEngine();
-    }
-    
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
+        super(4, 100, Color.black, "Volvo240");
     }
 
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
 
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
-    
+    @Override
     public double speedFactor(){
-        return enginePower * 0.01 * trimFactor;
+        return getEnginePower() * 0.01 * trimFactor;
     }
 
+    @Override
     public void incrementSpeed(double amount){
-	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
+	    setCurrentSpeed(Math.min(getCurrentSpeed() + speedFactor() * amount, getEnginePower()));
     }
 
+    @Override
     public void decrementSpeed(double amount){
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
+        setCurrentSpeed(Math.max(getCurrentSpeed() - speedFactor() * amount,0));
     }
 
     // TODO fix this method according to lab pm
@@ -64,5 +44,20 @@ public class Volvo240{
     // TODO fix this method according to lab pm
     public void brake(double amount){
         decrementSpeed(amount);
+    }
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void turnLeft() {
+
+    }
+
+    @Override
+    public void turnRight() {
+
     }
 }

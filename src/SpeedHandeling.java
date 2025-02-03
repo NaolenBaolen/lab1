38e79d0c267f731ae11116b22be629bcb7a4dd4b
NaolenBaolen <noelagustin@live.com>
2025-02-03 10:59:45 +0100

    added angle to trucks superclass, removed speedHandling interface

--- src/SpeedHandeling.java
+++
@@ -1,9 +0,0 @@
-public interface SpeedHandeling {
-
-    //following methods should be implemented in car subclasses but differently in each, hence the interface
-    void incrementSpeed(double amount);
-
-    void decrementSpeed(double amount);
-
-    double speedFactor();                       //for the speed handeling they have a speed factor that is specific for each car
-}
--- src/TruckVehicle.java
+++ src/TruckVehicle.java
@@ -1,18 +1,23 @@
 import java.awt.*;
 
 public abstract class TruckVehicle extends Vehicle{
-
+    //If both trucks should have angle on their beds, implement here and make transport raiseBed = al the way up and Scania arbitrary degrees.
+    //if not, implement angle in Scania subclass and up or down in Transport
     protected boolean bedRaised;
+    protected double bedAngle;
 
     public TruckVehicle (int nDoors, double enginePower, Color clr, String modelName){
         super (nDoors, enginePower, clr, modelName);
-        this.bedRaised = true;  //bed raised means we can move
+        this.bedRaised = true;
+        this.bedAngle = 0;     //bed raised means we can move (angle = 0)
     }
 
     @Override
     public double speedFactor(){        //maybe tweak to fit a trucks speedfactor, override if different trucks needs different speedfactors
         return getEnginePower() * 0.01;
     }
+
+    public double getBedAngle(){ return bedAngle;}
 
     //implemented differently in Scania and Transport (Maybe make one general or interface?? pros and cons??)
     public abstract void raiseBed();
